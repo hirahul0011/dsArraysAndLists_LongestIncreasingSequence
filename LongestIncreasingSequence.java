f04f0a8bca947d200bc2c2bfc779657c18a4b6db
hirahul0011 <ra_10001@yahoo.co.in>
2023-06-18 16:44:51 +0530

    Finalized Longest Increasing Sequence - Running in a loop

--- LongestIncreasingSequence.java
+++ LongestIncreasingSequence.java
@@ -8,26 +8,39 @@ public class LongestIncreasingSequence {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		
-		List<Integer> l1=new ArrayList<Integer>(); 
-		System.out.println("Please add five random numbers");
-		for(int i=1;i<=5;i++) {
-			Integer ni=sc.nextInt();
-			l1.add(ni);			
-		}		
-		int maximum=Integer.MIN_VALUE;
-		int lowerIndex=0;
-		int upperIndex=0;
-		for(int i=0;i<4;i++) {
-			if(l1.get(i+1)-l1.get(i)>maximum) {
-				maximum=l1.get(i+1)-l1.get(i);
-				lowerIndex=i;
-				upperIndex=i+1;
+		OperationsLoop:
+		do {
+			Scanner sc=new Scanner(System.in);
+			
+			List<Integer> l1=new ArrayList<Integer>(); 
+			System.out.println("Please add the count of numbers that you want to add and 0 to exit");
+			Integer cn=sc.nextInt();
+			if(cn>0 && cn<100) {			
+			
+				System.out.println("Please add "+cn+" random numbers");
+				for(int i=1;i<=cn;i++) {
+					Integer ni=sc.nextInt();
+					l1.add(ni);			
+				}		
+				int maximum=Integer.MIN_VALUE;
+				int lowerIndex=0;
+				int upperIndex=0;
+				for(int i=0;i<cn-1;i++) {
+					if(l1.get(i+1)-l1.get(i)>maximum) {
+						maximum=l1.get(i+1)-l1.get(i);
+						lowerIndex=i;
+						upperIndex=i+1;
+					}
+				}
+				System.out.println("Longest increment happened in the sequence is "+maximum);
+				System.out.println("And it happened between "+l1.get(lowerIndex)+" and "+l1.get(upperIndex));
+				
+			}else if(cn==0) {
+				break OperationsLoop;
+			}else {
+				System.out.println("Please add the count of numbers less than 100");
 			}
-		}
-		System.out.println("Longest increment happened in the sequence is "+maximum);
-		System.out.println("And it happened between "+l1.get(lowerIndex)+" and "+l1.get(upperIndex));
+		}while(1!=0);
 
 	}
 
